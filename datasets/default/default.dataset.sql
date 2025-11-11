2678ace2ef4ca1e2a7b7f291ce445a3c520025cc
Sean <sean@digitaldreams.ai>
2025-11-11 08:08:43 -0700

    Add LastName field to Account table and update related extract and mapping configurations

--- datasets/default/default.dataset.sql
+++ datasets/default/default.dataset.sql
@@ -5,7 +5,8 @@ CREATE TABLE "Account" (
 	"Type" VARCHAR(255), 
 	"Industry" VARCHAR(255), 
 	"Name" VARCHAR(255), 
+	"LastName" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Account" VALUES(1,'5551238765','Prospect','Biotechnology','Acme');
+INSERT INTO "Account" VALUES(1,'5551238765','Prospect','Biotechnology','Acme','Corporation');
 COMMIT;
